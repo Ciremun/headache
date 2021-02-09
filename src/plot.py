96e87fa80a3bbb8b805a51d8a39413fd38ff9aff
Ciremun <Ciremun@yandex.ru>
2021-02-09 15:58:16 +0300

    close #2

--- src/plot.py
+++ src/plot.py
@@ -10,23 +10,29 @@ def gen_plot(notes: List[Note]) -> None:
 
     x = []
     y = []
+    points = {}
     colors = {med: color for color, med in db.get_meds()}
-
-    for note in notes:
-        x.append(date2num(note.date))
-        y.append(note.points)
 
     fig = plt.figure()
     fig.set_size_inches(18.5, 10.5)
 
     graph = fig.add_subplot(111)
-    graph.scatter(x, y, c=[colors.get(note.med) or '#000000'
-                           for note in notes], edgecolors='#000000')
+
+    for note in notes:
+        x_s = date2num(note.date)
+        y_s = note.points
+        x.append(x_s)
+        y.append(y_s)
+        color = colors.get(note.med) or '#000000'
+        point = graph.scatter(x_s, y_s, c=color, edgecolors='#000000')
+        if points.get(note.med) is None:
+            points[note.med] = point
+
+    plt.legend(list(points.values()), list(points.keys()))
+
     graph.plot(x, y)
     graph.set_xticks(x)
     graph.set_xticklabels([note.date.strftime("%Y-%m-%d") for note in notes])
 
     plt.xticks(rotation=45)
     plt.savefig('flask/plot.png')
-
-# TODO(#2): colors legend
